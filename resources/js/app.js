bd1360d00215639e16c1c17bdf60d5d0c31e3e59
ilhamsj <ilhamsaputrajati@gmail.com>
2019-12-22 17:27:25 +0700

    vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,16 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
+
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+import VueAxios from "vue-axios";
+import Axios from "axios";
+Vue.use(VueAxios, Axios);
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +26,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +37,9 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
+// const app = new Vue({
+//     el: "#app"
+// });
+
+const router = new VueRouter({ mode: "history" });
+const app = new Vue(Vue.util.extend({ router })).$mount("#app");
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -5,19 +5,9 @@
                 <div class="card">
                     <div class="card-header">Example Component</div>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
+                    <div class="card-body">I'm an example component.</div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-
-<script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
-</script>
