05d51d787c42680a7b17b6abd9dfb299d67682e0
ilhamsj <ilhamsaputrajati@gmail.com>
2019-12-29 12:46:40 +0700

    faker

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'category_id' => random_int(1, 10),
-        'name' => $faker->name,
-        'price' => rand(1000, 10000),
+        'category_id'   => factory(\App\Category::class),
+        'name'          => $faker->name,
+        'price'         => rand(1000, 10000),
     ];
 });
--- database/factories/TransactionProductFactory.php
+++ database/factories/TransactionProductFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(TransactionProduct::class, function (Faker $faker) {
     return [
-        'product_id' => rand(1, 1),
-        'transaction_id' => rand(1, 1),
+        'product_id' => factory(\App\Product::class),
+        'transaction_id' => factory(\App\Transaction::class),
         'quantities' => rand(1, 10),
         'notes' => $faker->text(100),
     ];
